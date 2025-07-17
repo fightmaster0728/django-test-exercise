7115937f672c9041179790ab2a85c5f40d02c679
s1f10220288 <s1f102202886@iniad.org>
2025-07-17 14:09:50 +0900

    indexページにアイコンを追加。statusの関数が消えていたので再度作成しなおしました。

--- todo/views.py
+++ todo/views.py
@@ -4,19 +4,25 @@ from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
-# Create your views here.
 def index(request):
     if request.method == 'POST':
-        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task = Task(
+            title=request.POST['title'],
+            due_at=make_aware(parse_datetime(request.POST['due_at']))
+        )
         task.save()
 
-    if request.GET.get('order') == 'due':
+    order = request.GET.get('order')
+
+    if order == 'due':
         tasks = Task.objects.order_by('due_at')
+    elif order == 'status':
+        tasks = Task.objects.order_by('completed', '-posted_at')
     else:
         tasks = Task.objects.order_by('-posted_at')
 
     context = {
-        'tasks' : tasks
+        'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
 
@@ -27,14 +33,13 @@ def detail(request, task_id):
         raise Http404("Task does not exist")
     
     context = {
-        'task' : task,
+        'task': task,
     }
     return render(request, 'todo/detail.html', context)
 
-
 def delete(request, task_id):
     try:
-        task=Task.objects.get(pk=task_id)
+        task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
     task.delete()
@@ -48,20 +53,19 @@ def close(request, task_id):
     task.completed = True
     task.save()
     return redirect(index)
-    
+
 def update(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
     if request.method == 'POST':
-        task = Task.objects.get(pk=task_id)
         task.title = request.POST['title']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
         task.save()
         return redirect(detail, task_id)
+    
     context = {
         'task': task
     }
     return render(request, 'todo/edit.html', context)
-
